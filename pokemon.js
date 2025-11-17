094e2ed1eee2a7330014c7e725ead84087ad6216
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-11-17 18:49:25 +0000

    Add modal close button

--- pokemon.js
+++ pokemon.js
@@ -1,24 +1,16 @@
-export default async function getPokemons(count = 50) {
-  const pokemonFetchs = [];
-  for (let i = 0; i < count; i++) {
-    pokemonFetchs.push(
-      fetch(`https://pokeapi.co/api/v2/pokemon/${i + 1}/`).then((res) =>
-        res.json()
-      )
+export async function getPokemons(count = 50) {
+  try {
+    const pokemonsData = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/?limit=${count}`
     );
+    const pokemons = await pokemonsData.json();
+    return pokemons.results;
+  } catch (error) {
+    console.error(error);
   }
-  const pokemonDetails = await Promise.all(pokemonFetchs);
-  return extractDesiredData(pokemonDetails);
 }
 
-function extractDesiredData(pokemonDetails) {
-  return pokemonDetails.map(({ abilities, id, name, stats, sprites }) => {
-    return {
-      id,
-      name,
-      abilities,
-      stats,
-      image: sprites.front_default,
-    };
-  });
+export async function getPokemonDetails(id) {
+  const pokemonDetail = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  return await pokemonDetail.json();
 }
--- script.js
+++ script.js
@@ -1,21 +1,28 @@
-import getPokemons from "./pokemon.js";
+import { getPokemonDetails, getPokemons } from "./pokemon.js";
 const pokemonTable = document.querySelector("#pokemon-table");
 const tBody = document.querySelector("#pokemon-table tbody");
 const pokemonModal = document.querySelector("#pokemon-modal");
 const modalContent = document.querySelector("#modal-content");
+const closeModalBtn = document.querySelector("#close-modal-btn");
 
 async function createTable() {
-  const pokemonDetails = await getPokemons();
-  pokemonDetails.forEach((pokemon) => {
+  const pokemons = await getPokemons();
+  pokemons.forEach((pokemon, idx) => {
     const row = document.createElement("tr");
-    row.classList.add = "pokemon-row";
-    row.dataset.id = pokemon.id;
-    row.innerHTML = `<td><img src="${pokemon.image}" alt="${pokemon.name}"</td><td>${pokemon.id}</td><td>${pokemon.name}</td>`;
+    row.classList.add("pokemon-row");
+    row.dataset.id = pokemon.url.split("/").filter(Boolean).pop();
+    row.innerHTML = `<td><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url
+      .split("/")
+      .filter(Boolean)
+      .pop()}.png" alt="${pokemon.name}" /></td><td>${pokemon.url
+      .split("/")
+      .filter(Boolean)
+      .pop()}</td><td>${
+      pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)
+    }</td>`;
     tBody.appendChild(row);
   });
 }
-
-createTable();
 
 pokemonTable.addEventListener("click", (e) => {
   const clickedRow = e.target.closest("tr");
@@ -24,22 +31,27 @@ pokemonTable.addEventListener("click", (e) => {
   showPokemonDetails(id);
 });
 
-async function showPokemonDetails(id) {
-  const pokemonDetail = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${id}`
-  ).then((res) => res.json());
-  pokemonModal.classList.remove("hidden");
-  modalContent.innerHTML = `<div><img src="${
-    pokemonDetail.sprites.front_default
-  }" alt="${pokemonDetail.name}" /><div>${
-    pokemonDetail.name.charAt(0).toUpperCase() + pokemonDetail.name.slice(1)
-  }</div></div>`;
-  modalContent.innerHTML += `<div><div>Abilities:</div><div><ul>${pokemonDetail.abilities
-    .map((ability) => `<li>${ability.ability.name}</li>`)
-    .join("")}</ul></div></div>`;
-  console.log(pokemonDetail.abilities);
-}
-
 pokemonModal.addEventListener("click", () => {
   pokemonModal.classList.add("hidden");
 });
+
+modalContent.addEventListener("click", (e) => e.stopPropagation());
+
+closeModalBtn.addEventListener("click", () =>
+  pokemonModal.classList.add("hidden")
+);
+
+async function showPokemonDetails(id) {
+  const pokemonDetails = await getPokemonDetails(id);
+  pokemonModal.classList.remove("hidden");
+  const abilitiesHtml = `<div><div>Abilities:</div><div><ul>${pokemonDetails.abilities
+    .map((ability) => `<li>${ability.ability.name}</li>`)
+    .join("")}</ul></div></div>`;
+  modalContent.innerHTML = `<div><img src="${
+    pokemonDetails.sprites.front_default
+  }" alt="${pokemonDetails.name}" /><div>${
+    pokemonDetails.name.charAt(0).toUpperCase() + pokemonDetails.name.slice(1)
+  }</div></div>${abilitiesHtml}`;
+}
+
+createTable();
