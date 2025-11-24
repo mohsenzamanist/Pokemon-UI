3b7464ce0f4798262484f5c1c857fbbefa58b091
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-11-24 18:01:05 +0000

    Set width for images to fix performance

--- events/theme.js
+++ events/theme.js
@@ -1,6 +1,5 @@
 export function initTheme() {
   const themebtn = document.querySelector("#theme-toggle");
-  console.log(themebtn.innerHTML);
 
   themebtn.addEventListener("click", () => {
     const isDark = document.documentElement.classList.toggle("dark");
