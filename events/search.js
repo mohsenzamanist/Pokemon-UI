ed3d9c0482e63892a919769bddf5f88118d7b997
Mohsen Zamani <mohsenzamanist@gmail.com>
2025-11-25 13:10:53 +0000

    Add debouncing

--- events/search.js
+++ events/search.js
@@ -4,19 +4,42 @@ import { render } from "../render.js";
 // Initialises the search event on keyup event
 export function initSearch() {
   const searchInput = document.querySelector("#search-input");
-  if (!searchInput) {
+  const clearSearchBtn = document.querySelector("#clear-search");
+  if (!searchInput || !clearSearchBtn) {
     console.log("Search input field is not found!");
     return;
   }
 
-  searchInput.addEventListener("keyup", (e) => {
+  clearSearchBtn.addEventListener("click", () => {
+    searchInput.value = "";
+    clearSearchBtn.style.display = "none";
+    setCurrentList(getPokemonsState());
+    render();
+  });
+
+  searchInput.addEventListener("input", (e) => {
+    const searchTerm = e.target.value.trim().toLowerCase();
+    clearSearchBtn.style.display = searchTerm ? "inline" : "none";
+    searchPokemon(searchTerm);
+  });
+
+  const searchPokemon = debounce((text) => {
     const pokemons = getPokemonsState();
-    const searchTerm = e.target.value.toLowerCase();
     const filteredList = pokemons.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(searchTerm)
+      pokemon.name.toLowerCase().includes(text)
     );
     setCurrentList(filteredList);
     setSelectedPage(1);
     render();
   });
+
+  function debounce(cb, delay = 1000) {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => {
+        cb(...args);
+      }, delay);
+    };
+  }
 }
